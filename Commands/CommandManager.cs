8cac4facb2cf3bbee9c637c37addc14bf90d946d
ChobbyCode <jacobhadfield99@gmail.com>
2023-09-24 13:57:58 +0100

    Add project files.

---
+++ Commands/CommandManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BiscuitOS.Commands
+{
+    public class CommandManager
+    {
+        public void Command(string cmd)
+        {
+            try
+            {
+                // To Lower
+                var lw = cmd + " ";
+                try
+                {
+                    lw = cmd.ToLower();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Unable To Call c# Function .ToLower()");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                // Parse Command
+
+                string[] commandWord = lw.Split(' ');
+
+                /* 
+                 * Note: The only parsing in this file should be commandWord[0] and then they 
+                 * should go off to their own file.
+                 */
+                if (commandWord[0] == "exit")
+                {
+                    Cosmos.System.Power.Shutdown();
+                }
+                else
+                {
+                    if (commandWord[0] != "")
+                    {
+                        Console.WriteLine($"'{commandWord[0]}' is not recognized as an internal or external batch command.");
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please specify a component after command word.");
+            }
+        }
+
+    }
+}
---
+++ Kernel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Sys = Cosmos.System;
+using BiscuitOS.Commands;
+using Cosmos.System.Graphics;
+using System.Drawing;
+using System.Threading;
+
+namespace BiscuitOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        CommandManager commandManager = new CommandManager();
+        Canvas canvas;
+
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("Copyright ChobbyCode 2023.");
+            Console.WriteLine();
+
+            StartGUI();
+        }
+
+        protected override void Run()
+        {
+            //Text Stuff
+            //Console.Write("Biscuit> ");
+            //var input = Console.ReadLine();
+
+            //commandManager.Command(input);
+
+            DrawBox();
+
+            canvas.Display();
+        }
+
+        public void StartGUI()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Called GUI Start Function");
+
+            canvas = FullScreenCanvas.GetFullScreenCanvas();
+            canvas.Clear(Color.Blue);
+
+            Console.WriteLine("Loading Canvas Mode");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Thread.Sleep(500);
+
+            canvas.Display();
+        }
+
+        public void DrawBox()
+        {
+            Pen pen = new Pen(Color.Black);
+
+            FillRect(pen, 10, 10, 600, 450);
+        }
+
+        public void FillRect(Pen pen, int startX, int startY, int width, int height)
+        {
+            canvas.DrawFilledRectangle(pen, startY, startY, width, height);
+        }
+    }
+}
