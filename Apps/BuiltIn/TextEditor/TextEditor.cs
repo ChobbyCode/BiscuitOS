cb46eac2f7ba89df294d1dc51a0b7ded985c0241
ChobbyCode <jacobhadfield99@gmail.com>
2023-11-08 18:49:01 +0000

    I can't find the issue, so time to rewrite all the code

--- Apps/BuiltIn/TextEditor/TextEditor.cs
+++ Apps/BuiltIn/TextEditor/TextEditor.cs
@@ -8,12 +8,18 @@ namespace BiscuitOS.Apps.TextEditor
     public class TextEditor
     {
         public static void StartTextEditor(string[] filePath)
-        { 
-            // Start The Editor
-            string[] file = File.ReadAllLines(filePath[0]);
+        {
+            try
+            {
+                // Start The Editor
+                string[] file = File.ReadAllLines(filePath[0]);
 
-            RenderScreen(file.ToArray(), 0, 1);
-            TextEditorM(file, filePath[0]);
+                RenderScreen(file.ToArray(), 0, 1);
+                TextEditorM(file, filePath[0]);
+            }catch (Exception ex)
+            {
+                BConsole.WriteLine(ex.Message);
+            }
         }
 
         private static void TextEditorM(string[] file, string path)
